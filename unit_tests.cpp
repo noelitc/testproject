a3dd5d79a6938d2c07b5451a152d8cf8bfaa2aed
Noel O Hara <22346864+noelitc@users.noreply.github.com>
2019-08-15 12:54:42 +0100

    Update unit_tests.cpp

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,43 +1,61 @@
-#include "project.h"
+//#include "project.h"
+#include "gtest/gtest.h"
+#include <string>
+#include <iostream>
 
-#include <gtest/gtest.h>
-
+struct Project
+{
+	static void Foo()
+	{
+		std::cout << "hello world" << std::endl;
+	}
+};
 TEST(test, empty)
 {
-  Project::Foo();
-  Test()
+	Project::Foo();
+	
 }
 
-int array[10];
-void Fillarray()
+
+void Fillarray( int array[10])
 {
 	for (int i = 0; i < 10; i++)
 	{
 		array[i] = i;
 	}
 }
-TEST(testArray,emptyArray) {
-	
-	int correctArray[] = {0,0,0,0,0,0,0,0,0 };
+void Initarray(int array[10])
+{
 	for (int i = 0; i < 10; i++)
 	{
-		EXPECT_EQ(array[i], correctArray[i]) << "position  " << i << "is incorrect";
+		array[i] = 0;
+	}
+}
+TEST(testArray, emptyArray) {
+
+	int testArray[10];
+	Initarray(testArray);
+	int correctArray[] = { 0,0,0,0,0,0,0,0,0,0 };
+	for (int i = 0; i < 10; i++)
+	{
+		EXPECT_EQ(testArray[i], correctArray[i]) << "position  " << i << "is incorrect";
 
 	}
- 
-  EXPECT_TRUE(true);
-  
+
+	EXPECT_TRUE(true);
+
 }
 
-TEST(testArray,filledArray) {
-	Fillarray();
-	int correctArray[] = { 1,1,2,3,4,5,6,7,8,9 };
+TEST(testArray, filledArray) {
+	int testArray[10];
+	Fillarray(testArray);
+	int correctArray[] = { 0,1,2,3,4,5,6,7,8,9 };
 	for (int i = 0; i < 10; i++)
 	{
-		EXPECT_EQ(array[i], correctArray[i]) << "position  " << i << "is incorrect";
+		EXPECT_EQ(testArray[i], correctArray[i]) << "position  " << i << "is incorrect";
 
 	}
- 
-  EXPECT_TRUE(true);
-  
+
+	EXPECT_TRUE(true);
+
 }
