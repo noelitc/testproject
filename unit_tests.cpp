45be055c00397029aaeb5c8e8b2f54c98cc47af9
Noel O Hara <22346864+noelitc@users.noreply.github.com>
2019-08-15 12:43:27 +0100

    Update unit_tests.cpp

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -5,4 +5,39 @@
 TEST(test, empty)
 {
   Project::Foo();
+  Test()
+}
+
+int array[10];
+void Fillarray()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		array[i] = i;
+	}
+}
+TEST(testArray,emptyArray) {
+	
+	int correctArray[] = {0,0,0,0,0,0,0,0,0 };
+	for (int i = 0; i < 10; i++)
+	{
+		EXPECT_EQ(array[i], correctArray[i]) << "position  " << i << "is incorrect";
+
+	}
+ 
+  EXPECT_TRUE(true);
+  
+}
+
+TEST(testArray,filledArray) {
+	Fillarray();
+	int correctArray[] = { 1,1,2,3,4,5,6,7,8,9 };
+	for (int i = 0; i < 10; i++)
+	{
+		EXPECT_EQ(array[i], correctArray[i]) << "position  " << i << "is incorrect";
+
+	}
+ 
+  EXPECT_TRUE(true);
+  
 }
